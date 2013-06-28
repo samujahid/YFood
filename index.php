bdf99915692ff4bc98ab4fdaa3364703315bbe7f
mujahid <mujahid@webaroo.com>
2013-06-28 19:53:50 +0530

    Directory Structure

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Your Food</title>
+</head>
+
+<body>
+<?php 
+include("common/header.php");
+include("common/left-bar.php");
+include("common/content.php");
+include("common/right-bar.php");
+include("common/footer.php");
+?>
+</body>
+</html>
